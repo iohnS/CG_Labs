1e9778ad65e40308c821470830b99c8075481fa7
iohnS <john02sun@gmail.com>
2024-09-26 15:48:44 +0200

    assignment 2 done

--- src/EDAF80/assignment2.cpp
+++ src/EDAF80/assignment2.cpp
@@ -18,16 +18,16 @@
 #include <cstdlib>
 #include <stdexcept>
 
-edaf80::Assignment2::Assignment2(WindowManager& windowManager) :
-	mCamera(0.5f * glm::half_pi<float>(),
-	        static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
-	        0.01f, 1000.0f),
-	inputHandler(), mWindowManager(windowManager), window(nullptr)
+edaf80::Assignment2::Assignment2(WindowManager &windowManager) : mCamera(0.5f * glm::half_pi<float>(),
+																		 static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
+																		 0.01f, 1000.0f),
+																 inputHandler(), mWindowManager(windowManager), window(nullptr)
 {
-	WindowManager::WindowDatum window_datum{ inputHandler, mCamera, config::resolution_x, config::resolution_y, 0, 0, 0, 0};
+	WindowManager::WindowDatum window_datum{inputHandler, mCamera, config::resolution_x, config::resolution_y, 0, 0, 0, 0};
 
 	window = mWindowManager.CreateGLFWWindow("EDAF80: Assignment 2", window_datum, config::msaa_rate);
-	if (window == nullptr) {
+	if (window == nullptr)
+	{
 		throw std::runtime_error("Failed to get a window: aborting!");
 	}
 
@@ -39,16 +39,21 @@ edaf80::Assignment2::~Assignment2()
 	bonobo::deinit();
 }
 
-void
-edaf80::Assignment2::run()
+void edaf80::Assignment2::run()
 {
 	// Load the sphere geometry
-	auto const shape = parametric_shapes::createCircleRing(2.0f, 0.75f, 40u, 4u);
+	// auto const shape = parametric_shapes::createCircleRing(2.0f, 0.75f, 40u, 4u);
+	// auto const shape = parametric_shapes::createQuad(0.25f, 0.15f);
+	auto const shape = parametric_shapes::createSphere(0.15f, 10u, 10u);
 	if (shape.vao == 0u)
 		return;
 
 	// Set up the camera
 	mCamera.mWorld.SetTranslate(glm::vec3(0.0f, 1.0f, 9.0f));
+
+	// mCamera.mWorld.SetTranslate(glm::vec3(0.0f, -0.5f, 0.0f));
+	// mCamera.mWorld.SetRotateX(glm::half_pi<float>());
+
 	mCamera.mMouseSensitivity = glm::vec2(0.003f);
 	mCamera.mMovementSpeed = glm::vec3(3.0f); // 3 m/s => 10.8 km/h
 
@@ -56,56 +61,58 @@ edaf80::Assignment2::run()
 	ShaderProgramManager program_manager;
 	GLuint fallback_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Fallback",
-	                                         { { ShaderType::vertex, "common/fallback.vert" },
-	                                           { ShaderType::fragment, "common/fallback.frag" } },
-	                                         fallback_shader);
-	if (fallback_shader == 0u) {
+											 {{ShaderType::vertex, "common/fallback.vert"},
+											  {ShaderType::fragment, "common/fallback.frag"}},
+											 fallback_shader);
+	if (fallback_shader == 0u)
+	{
 		LogError("Failed to load fallback shader");
 		return;
 	}
 
 	GLuint diffuse_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Diffuse",
-	                                         { { ShaderType::vertex, "EDAF80/diffuse.vert" },
-	                                           { ShaderType::fragment, "EDAF80/diffuse.frag" } },
-	                                         diffuse_shader);
+											 {{ShaderType::vertex, "EDAF80/diffuse.vert"},
+											  {ShaderType::fragment, "EDAF80/diffuse.frag"}},
+											 diffuse_shader);
 	if (diffuse_shader == 0u)
 		LogError("Failed to load diffuse shader");
 
 	GLuint normal_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Normal",
-	                                         { { ShaderType::vertex, "EDAF80/normal.vert" },
-	                                           { ShaderType::fragment, "EDAF80/normal.frag" } },
-	                                         normal_shader);
+											 {{ShaderType::vertex, "EDAF80/normal.vert"},
+											  {ShaderType::fragment, "EDAF80/normal.frag"}},
+											 normal_shader);
 	if (normal_shader == 0u)
 		LogError("Failed to load normal shader");
 
 	GLuint tangent_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Tangent",
-	                                         { { ShaderType::vertex, "EDAF80/tangent.vert" },
-	                                           { ShaderType::fragment, "EDAF80/tangent.frag" } },
-	                                         tangent_shader);
+											 {{ShaderType::vertex, "EDAF80/tangent.vert"},
+											  {ShaderType::fragment, "EDAF80/tangent.frag"}},
+											 tangent_shader);
 	if (tangent_shader == 0u)
 		LogError("Failed to load tangent shader");
 
 	GLuint binormal_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Bitangent",
-	                                         { { ShaderType::vertex, "EDAF80/binormal.vert" },
-	                                           { ShaderType::fragment, "EDAF80/binormal.frag" } },
-	                                         binormal_shader);
+											 {{ShaderType::vertex, "EDAF80/binormal.vert"},
+											  {ShaderType::fragment, "EDAF80/binormal.frag"}},
+											 binormal_shader);
 	if (binormal_shader == 0u)
 		LogError("Failed to load binormal shader");
 
 	GLuint texcoord_shader = 0u;
 	program_manager.CreateAndRegisterProgram("Texture coords",
-	                                         { { ShaderType::vertex, "EDAF80/texcoord.vert" },
-	                                           { ShaderType::fragment, "EDAF80/texcoord.frag" } },
-	                                         texcoord_shader);
+											 {{ShaderType::vertex, "EDAF80/texcoord.vert"},
+											  {ShaderType::fragment, "EDAF80/texcoord.frag"}},
+											 texcoord_shader);
 	if (texcoord_shader == 0u)
 		LogError("Failed to load texcoord shader");
 
 	auto const light_position = glm::vec3(-2.0f, 4.0f, 2.0f);
-	auto const set_uniforms = [&light_position](GLuint program){
+	auto const set_uniforms = [&light_position](GLuint program)
+	{
 		glUniform3fv(glGetUniformLocation(program, "light_position"), 1, glm::value_ptr(light_position));
 	};
 
@@ -128,41 +135,38 @@ edaf80::Assignment2::run()
 	auto circle_rings = Node();
 	circle_rings.set_geometry(shape);
 	circle_rings.set_program(&fallback_shader, set_uniforms);
-	TRSTransformf& circle_rings_transform_ref = circle_rings.get_transform();
-
+	TRSTransformf &circle_rings_transform_ref = circle_rings.get_transform();
 
 	//! \todo Create a tesselated sphere and a tesselated torus
-
 
 	glClearDepthf(1.0f);
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
 
-
 	auto const control_point_sphere = parametric_shapes::createSphere(0.1f, 10u, 10u);
 	std::array<glm::vec3, 9> control_point_locations = {
-		glm::vec3( 0.0f,  0.0f,  0.0f),
-		glm::vec3( 1.0f,  1.8f,  1.0f),
-		glm::vec3( 2.0f,  1.2f,  2.0f),
-		glm::vec3( 3.0f,  3.0f,  3.0f),
-		glm::vec3( 3.0f,  0.0f,  3.0f),
-		glm::vec3(-2.0f, -1.0f,  3.0f),
+		glm::vec3(0.0f, 0.0f, 0.0f),
+		glm::vec3(1.0f, 1.8f, 1.0f),
+		glm::vec3(2.0f, 1.2f, 2.0f),
+		glm::vec3(3.0f, 3.0f, 3.0f),
+		glm::vec3(3.0f, 0.0f, 3.0f),
+		glm::vec3(-2.0f, -1.0f, 3.0f),
 		glm::vec3(-3.0f, -3.0f, -3.0f),
 		glm::vec3(-2.0f, -1.2f, -2.0f),
-		glm::vec3(-1.0f, -1.8f, -1.0f)
-	};
+		glm::vec3(-1.0f, -1.8f, -1.0f)};
 	std::array<Node, control_point_locations.size()> control_points;
-	for (std::size_t i = 0; i < control_point_locations.size(); ++i) {
-		auto& control_point = control_points[i];
+	for (std::size_t i = 0; i < control_point_locations.size(); ++i)
+	{
+		auto &control_point = control_points[i];
 		control_point.set_geometry(control_point_sphere);
 		control_point.set_program(&diffuse_shader, set_uniforms);
 		control_point.get_transform().SetTranslate(control_point_locations[i]);
 	}
 
-
 	auto lastTime = std::chrono::high_resolution_clock::now();
 
 	std::int32_t program_index = 0;
+	std::int32_t curr_idx = 0;
 	float elapsed_time_s = 0.0f;
 	auto cull_mode = bonobo::cull_mode_t::disabled;
 	auto polygon_mode = bonobo::polygon_mode_t::fill;
@@ -174,12 +178,13 @@ edaf80::Assignment2::run()
 
 	changeCullMode(cull_mode);
 
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		auto const nowTime = std::chrono::high_resolution_clock::now();
 		auto const deltaTimeUs = std::chrono::duration_cast<std::chrono::microseconds>(nowTime - lastTime);
 		lastTime = nowTime;
 
-		auto& io = ImGui::GetIO();
+		auto &io = ImGui::GetIO();
 		inputHandler.SetUICapture(io.WantCaptureMouse, io.WantCaptureKeyboard);
 
 		glfwPollEvents();
@@ -194,7 +199,6 @@ edaf80::Assignment2::run()
 		if (inputHandler.GetKeycodeState(GLFW_KEY_F11) & JUST_RELEASED)
 			mWindowManager.ToggleFullscreenStatusForWindow(window);
 
-
 		// Retrieve the actual framebuffer size: for HiDPI monitors,
 		// you might end up with a framebuffer larger than what you
 		// actually asked for. For example, if you ask for a 1920x1080
@@ -208,42 +212,68 @@ edaf80::Assignment2::run()
 
 		mWindowManager.NewImGuiFrame();
 
-
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 		bonobo::changePolygonMode(polygon_mode);
 
-
-		if (interpolate) {
+		if (interpolate)
+		{
 			//! \todo Interpolate the movement of a shape between various
 			//!        control points.
-			if (use_linear) {
+			if (curr_idx == control_point_locations.size())
+			{
+				curr_idx = 0;
+			}
+
+			if(elapsed_time_s > 1.0f)
+			{
+				elapsed_time_s = 0.0f;
+				curr_idx++;
+			}
+
+			glm::vec3 newPosition = glm::vec3(0.0f);
+			if (use_linear)
+			{
+				newPosition = interpolation::evalLERP(control_point_locations[curr_idx], control_point_locations[curr_idx + 1 % control_point_locations.size()], elapsed_time_s);
 				//! \todo Compute the interpolated position
 				//!       using the linear interpolation.
 			}
-			else {
+			else
+			{
+				newPosition = interpolation::evalCatmullRom(control_point_locations[curr_idx], 
+																		control_point_locations[curr_idx + 1 % control_point_locations.size()], 
+																		control_point_locations[curr_idx + 2 % control_point_locations.size()], 
+																		control_point_locations[curr_idx + 3 % control_point_locations.size()], 
+																		catmull_rom_tension, 
+																		elapsed_time_s);
 				//! \todo Compute the interpolated position
 				//!       using the Catmull-Rom interpolation;
 				//!       use the `catmull_rom_tension`
 				//!       variable as your tension argument.
 			}
+			circle_rings_transform_ref.SetTranslate(newPosition);
 		}
 
 		circle_rings.render(mCamera.GetWorldToClipMatrix());
-		if (show_control_points) {
-			for (auto const& control_point : control_points) {
+		if (show_control_points)
+		{
+			for (auto const &control_point : control_points)
+			{
 				control_point.render(mCamera.GetWorldToClipMatrix());
 			}
 		}
 
 		bool const opened = ImGui::Begin("Scene Controls", nullptr, ImGuiWindowFlags_None);
-		if (opened) {
+		if (opened)
+		{
 			auto const cull_mode_changed = bonobo::uiSelectCullMode("Cull mode", cull_mode);
-			if (cull_mode_changed) {
+			if (cull_mode_changed)
+			{
 				changeCullMode(cull_mode);
 			}
 			bonobo::uiSelectPolygonMode("Polygon mode", polygon_mode);
 			auto selection_result = program_manager.SelectProgram("Shader", program_index);
-			if (selection_result.was_selection_changed) {
+			if (selection_result.was_selection_changed)
+			{
 				circle_rings.set_program(selection_result.program, set_uniforms);
 			}
 			ImGui::Separator();
@@ -275,10 +305,13 @@ int main()
 
 	Bonobo framework;
 
-	try {
+	try
+	{
 		edaf80::Assignment2 assignment2(framework.GetWindowManager());
 		assignment2.run();
-	} catch (std::runtime_error const& e) {
+	}
+	catch (std::runtime_error const &e)
+	{
 		LogError(e.what());
 	}
 }
--- src/EDAF80/interpolation.cpp
+++ src/EDAF80/interpolation.cpp
@@ -3,15 +3,23 @@
 glm::vec3
 interpolation::evalLERP(glm::vec3 const& p0, glm::vec3 const& p1, float const x)
 {
-	//! \todo Implement this function
-	return glm::vec3();
+	glm::vec3 result = (1-x)*p0 + x*p1;
+	return result;
 }
 
 glm::vec3
 interpolation::evalCatmullRom(glm::vec3 const& p0, glm::vec3 const& p1,
                               glm::vec3 const& p2, glm::vec3 const& p3,
                               float const t, float const x)
-{
-	//! \todo Implement this function
-	return glm::vec3();
+{	
+	glm::vec4 x_vec = glm::vec4(1, x, x*x, x*x*x);
+
+	glm::mat4 t_mat = glm::mat4(glm::vec4(0, 1, 0, 0), 
+								glm::vec4(-t, 0, t, 0), 
+								glm::vec4(2*t, t-3, 3-2*t, -t), 
+								glm::vec4(-t, 2-t, t-2, t));
+								
+	glm::mat4 p_vec = glm::mat4(glm::vec4(p0, 0), glm::vec4(p1, 0), glm::vec4(p2, 0), glm::vec4(p3, 0));
+
+	return p_vec * t_mat * x_vec;
 }
